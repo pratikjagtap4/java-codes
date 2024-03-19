d4857c6338b2eeae18fad220f2340762b0d05a96
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-19 09:00:07 +0530

    static method and jvm compilation

---
+++ 06static And Method/$15_static_Method1.java
@@ -0,0 +1,14 @@
+class $15_static_Method1
+{
+	static float b = 200.5f;
+
+	public static void main(String [] args)
+	{
+		int a = 100;
+		System.out.println(a);
+		
+		System.out.println(b);
+		System.out.println($15_static_Method1.b);
+	}
+}
+
