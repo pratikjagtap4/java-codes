bcfb9426e6acb192e00e4e9b89e9f939afa4dee6
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-19 09:07:07 +0530

    for , while and do while loop in java

--- 05loop/$06_for6.java
+++ 05loop/$06_for6.java
@@ -2,9 +2,11 @@ class $06_for6
 {
 	public static void main(String[] args)
 	{
-		for(int i = 1; System.out.println("hello"); i++)
+		// for(int i = 1; System.out.println("hello"); i++)
 		{
-			System.out.println(i);
+			// System.out.println(i);
 		}
 	}
 }
+
+//  statements can be used within condition of for , while or do while loop
