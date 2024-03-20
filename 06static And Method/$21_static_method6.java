39e2b60abace4a330baf69dc6cff475fef3aaf79
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-20 13:15:17 +0530

    staic variables and static methods

---
+++ 06static And Method/$21_static_method6.java
@@ -0,0 +1,15 @@
+class $21_static_method6
+{
+	public static void main(String[] args)
+	{
+		Demo3.m1();
+	}
+}
+
+class Demo3
+{
+	static void m1()
+	{
+		System.out.println("inside m1 method");
+	}
+}
