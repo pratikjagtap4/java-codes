af7679b542da189caa44cf5e9ea3166c25d24629
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-24 09:52:36 +0530

    bg changer and onclick function

---
+++ 07non-static And Method/example32.java
@@ -0,0 +1,28 @@
+class sample32
+{
+	static int a = 100;
+	float b = 200.5f;
+}
+
+class example32
+{
+
+	public static void main(String[] args)
+	{
+		example32 obj = new example32();
+		obj.m1();
+		example32.m2();
+	}
+
+	void m1()
+	{
+		sample32 obj = new sample32();
+		System.out.println( sample32.a + obj.b);
+	}
+
+	static void m2()
+	{
+		sample32 obj = new sample32();
+		System.out.println( sample32.a + obj.b);
+	}
+}
