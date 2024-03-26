a8301d67a0e66dd2f014306961a1bffb7f77aef3
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-26 23:23:19 +0530

    non primitive variables

---
+++ 08nonPrimitive/example38.java
@@ -0,0 +1,34 @@
+class example38
+{
+	//static byte a;
+	//static short b;
+	//static int c;
+	//static long d;
+	//static float e;
+	//static double f;
+	//static char h;
+	//static boolean i;
+
+	 byte a;
+	 short b;
+	 int c;
+	 long d;
+	 float e;
+	 double f;
+	 char h;
+	 boolean i;
+
+	public static void main(String[] args)
+	{
+		example38 obj = new example38();
+
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		System.out.println(obj.c);
+		System.out.println("d " + obj.d);
+		System.out.println(obj.e);
+		System.out.println(obj.f);
+		System.out.println(obj.h);
+		System.out.println(obj.i);
+	}
+}
