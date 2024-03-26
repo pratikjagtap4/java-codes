17fb248461ca8b6521e8c839fca763158d9d29a0
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-26 21:41:38 +0530

    non static variables and non static methods

---
+++ 07non-static And Method/example33a.java
@@ -0,0 +1,16 @@
+class example33a
+{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		m1(a);
+		System.out.println(a);
+	}
+
+	static void m1(int a)
+	{
+		a = a +1;
+		//return a;
+	}
+
+}
