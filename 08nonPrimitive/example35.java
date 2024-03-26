d3f198a623997b20b28c92adc59020cadbeb2e2f
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-26 21:52:32 +0530

    non primitive variables

---
+++ 08nonPrimitive/example35.java
@@ -0,0 +1,19 @@
+class example35
+{
+	int  a = 1000;
+
+	public static void main(String[] args)
+	{
+		example35 obj = new example35();
+
+		System.out.println(obj.a);
+
+		m1(obj);
+
+
+	}
+	static void m1(example35 obj)
+	{
+		System.out.println(obj.a);
+	}
+}
