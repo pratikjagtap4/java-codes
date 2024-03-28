6e4c304c4360555da19775e31af46fef112b1292
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-28 23:22:09 +0530

    non primitive reference variables

---
+++ 08nonPrimitive/example53.java
@@ -0,0 +1,11 @@
+class example53
+{
+	int a = 100;
+	public static void main(String[] args)
+	{
+		example53 obj = new example53();
+		System.out.println(obj.a);
+
+		System.out.println(new example53().a);
+	}
+}
