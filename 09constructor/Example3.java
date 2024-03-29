da56e8456ef170f414b1d2ec792a27ad845724ed
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-29 23:29:54 +0530

    constructor

---
+++ 09constructor/Example3.java
@@ -0,0 +1,17 @@
+class Example3
+{
+	Example3()
+	{
+		System.out.println("inside Constructor");
+	}
+
+	Example3()
+	{
+	System.out.println("inside constructor");
+	}
+
+	public static void main(String[] args)
+	{
+		Example3 obj = new Example3();
+	}
+}
