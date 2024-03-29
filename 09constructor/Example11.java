50b63c4a0e2194c14095736f2b9cb5aca9b0e709
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-03-29 23:57:33 +0530

    constructor

---
+++ 09constructor/Example11.java
@@ -0,0 +1,12 @@
+class Example11
+{
+	Example11(int a)
+	{
+		System.out.println("inside 1 arg constructor");
+	}
+
+	public static void main(String[] args)
+	{
+		Example11 obj = new Example11();
+	}
+}
