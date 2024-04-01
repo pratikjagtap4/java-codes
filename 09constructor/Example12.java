7fca31f158b2fefedbf7512b7df041cd6fbdc51f
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-01 23:09:26 +0530

    constructor

--- 09constructor/Example12.java
+++ 09constructor/Example12.java
@@ -1,1 +1,21 @@
 class Example12
+{
+	Example12()
+	{
+		System.out.println("inside 0 args");
+	}
+	Example12(int a)
+	{
+		System.out.println("inside 1 args");
+	}
+	void m1()
+	{
+		System.out.println("inside m1 method");
+	}
+
+	public static void main(String[] args)
+	{
+		new Example12().m1();
+		new Example12(10).m1();
+	}
+}
