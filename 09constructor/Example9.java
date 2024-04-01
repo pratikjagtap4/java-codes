a105a0bb4000de5d5050aaa479c040ac31da1b1b
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-01 23:19:05 +0530

    constructor

---
+++ 09constructor/Example9.java
@@ -0,0 +1,39 @@
+class HDFC
+{
+	int accBal;
+	HDFC(int accBal)
+	{
+		System.out.println("welcome hdfc bank , your account is getting started");
+		this.accBal = accBal;
+	}
+
+	void getBalance()
+	{
+		System.out.println("your account balance is" + accBal);
+	}
+
+	void transferMoney(int transferMoney)
+	{
+		if(transferMoney <= accBal)
+		{
+			System.out.println( transferMoney + "is transfer succesfully");
+			accBal = accBal - transferMoney ;
+		}
+		else
+		{
+			System.out.println("insufficient balance");
+		}
+	}
+}
+
+class Example9
+{
+	public static void main(String[] args)
+	{
+		HDFC cust1 = new HDFC(5000);
+
+		cust1.getBalance();
+		cust1.transferMoney(3000);
+		cust1.getBalance();
+	}
+}
