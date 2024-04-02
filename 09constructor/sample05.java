72621db338704e507c3a528cf2198eadcb4671b7
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-02 23:30:44 +0530

    constructor

---
+++ 09constructor/sample05.java
@@ -0,0 +1,7 @@
+class sample05
+{
+	public static void main(String [] args)
+	{
+		System.out.println("hello");
+	}
+}
---
+++ 09constructor/sample06.java
@@ -0,0 +1,7 @@
+class sample06
+{
+	public static void main(String [] args)
+	{
+		System.out.println("hello");
+	}
+}
