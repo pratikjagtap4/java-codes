6b8ea7bf84e044253254159f9cb09364c25b7bbe
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-02 12:20:12 +0530

    constructor

---
+++ 09constructor/Example14.java
@@ -0,0 +1,17 @@
+class Example14
+{
+	Example14()
+	{
+		this(10);
+		System.out.println("inside 0 args");
+	}
+	Example14(int a)
+	{
+		System.out.println("inside 1 args");
+	}
+
+	public static void main(String[] args)
+	{
+		Example14 obj  = new Example14();
+	}
+}
