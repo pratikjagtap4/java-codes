ff5d6037b1855682b1db1af5eae63e29c646242a
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-04 08:40:27 +0530

    constructor

---
+++ 10blocks/Example06.java
@@ -0,0 +1,25 @@
+class Example06
+{
+	int a;
+	{
+		System.out.println("inside instance block " + a);
+		a = 10;
+	}
+
+	Example06()
+	{
+		System.out.println("inside constructor " +a);
+		a = 100;
+	}
+	void m1()
+	{
+		System.out.println("inside m1 " + a);
+		a =100;
+	}
+	public static void main(String[] args)
+	{
+		Example06 obj = new Example06();
+		obj.m1();
+		System.out.println(obj.a);
+	}
+}
