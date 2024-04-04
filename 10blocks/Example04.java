5febf20fdefa21aa137747636fc6b69adcf220d1
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-04 08:27:33 +0530

    constructor

---
+++ 10blocks/Example04.java
@@ -0,0 +1,12 @@
+class Example04
+{
+	{ 
+		System.out.println("insdie instamce block - 1");
+	}
+
+	public static void main(String[] args)
+	{
+		Example04 obj = new Example04();
+		Example04 obj2 =  new Example04();
+	}
+}
