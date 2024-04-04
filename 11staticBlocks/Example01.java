6dda355e881da6ede3603c37a8d4c726ddcac4c5
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-04 23:35:12 +0530

    static blocks

---
+++ 11staticBlocks/Example01.java
@@ -0,0 +1,12 @@
+class Example01
+{
+	static 
+	{
+		System.out.println("inside static blocks");
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("start");
+		System.out.println("Stop");
+	}
+}
