2563d7ee27e5c6cda5898e8abd603b5411745127
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-05 23:28:35 +0530

    instance block

---
+++ 10instanceBlocks/Example12.java
@@ -0,0 +1,17 @@
+class sample
+{
+	{
+		System.out.println("hello");
+	}
+}
+
+class Example12
+{
+	{
+		sample obj = new sample();
+	}
+	public static void main(String[] args)
+	{
+		Example12 obj = new Example12();
+	}
+}
