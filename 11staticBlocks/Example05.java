5672c319fca43a78aa0ac41530e96ad4a4fd00a4
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-06 00:08:23 +0530

    static  block

--- 11staticBlocks/Example05.java
+++ 11staticBlocks/Example05.java
@@ -1,14 +1,18 @@
 class HDFC
 {
 	int accBal;
+
 	static{
 		System.out.println("Connecting to server");
 	}
+
+
 	{
 		System.out.println("Logics to connect and KYC verification");
 	}
-	HDFC(int accBal)
-	{
+
+
+	HDFC(int accBal){
 		if(accBal >= 5000)
 		{
 			System.out.println("your account is getting created");
@@ -25,6 +29,20 @@ class HDFC
 	{
 		System.out.println("account balance is" + accBal);
 	}
+
+	void transferMoney(int amount)
+	{
+		if(amount <=accBal)
+		{
+			System.out.println(amount + " transfer successfully" );
+			this.accBal = this.accBal - amount;
+		}
+		else 
+		{
+			System.out.println("insufficient balance");
+		}
+	}
+
 
 }
 
