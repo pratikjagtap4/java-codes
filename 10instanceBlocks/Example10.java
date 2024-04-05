eb3a5e3825f1a5c6d5c600aea5432387ba9f5ad8
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-05 23:25:54 +0530

    instance block

---
+++ 10instanceBlocks/Example10.java
@@ -0,0 +1,13 @@
+class Example10
+{
+	
+	Example10()
+	{
+		System.out.println("constructor");
+		Example10 obj = new Example10();
+	}
+	public static void main(String[] args)
+	{
+		Example10 obj = new Example10();
+	}
+}
