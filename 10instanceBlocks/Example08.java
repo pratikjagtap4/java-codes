aef5dc26b402fb7db03af76c32b1845cbc989f1a
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-05 23:25:20 +0530

    instance block

--- 10instanceBlocks/Example08.java
+++ 10instanceBlocks/Example08.java
@@ -1,17 +1,23 @@
 class Example08
 {
-	static int a;
+	static int a=1;
+
+
 	{
 		a = 10;
 		System.out.println("inside instance block " + a);
 	}
-	Example08()
+	
+	Example08(int a)
 	{
-		a = 100;
-		System.out.println("inside constructor " +a);
+		System.out.println("inside constructor" + this.a);
+		this.a = a;
+		System.out.println("inside constructor " + this.a);
 	}
+
 	public static void main(String[] args)
 	{
-		Example08 obj = new Example08();
+		Example08 obj = new Example08(100);
 	}
+
 }
