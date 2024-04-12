796e0c183b7f718de30bab3bd4505d75dd87f12a
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-12 15:29:49 +0530

    inheritance

---
+++ 12inheritance/Example06.java
@@ -0,0 +1,32 @@
+/*class sample1
+{
+	void m1()
+	{
+		System.out.println("m1 method");
+	}
+}
+
+class sample2
+{
+	void m1()
+	{
+		System.out.println("m2 method");
+	}
+}
+
+class Demo extends sample1 , sample2
+{
+	void m2()
+	{
+		System.out.println("inside m2 method");
+		m1();
+	}
+}
+class Example06
+{
+	public static void main(String[] args)
+	{
+		Demo obj = new Demo();
+		obj.m2();
+	}
+}*/
