0a28f7ac8943c271ac73886fe0d88e2907b113ea
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-26 21:34:41 +0530

    overloading

---
+++ 13polymorphism/Example11.java
@@ -0,0 +1,9 @@
+class Example11
+{
+	public static void main(String[] args)
+	{
+		System.out.println(10+20);
+		System.out.println("hello"+"world");
+		System.out.println(10+20+"hello" + 30+40);
+	}
+}
