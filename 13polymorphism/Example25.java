5c417ed172dbb743114678f3b5b1f348b1125b74
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-04-28 22:33:40 +0530

    overloading

---
+++ 13polymorphism/Example25.java
@@ -0,0 +1,27 @@
+class Demo
+{
+	int a = 100;
+	void m1()
+	{
+		System.out.println("parent  method" + this.a);
+	}
+}
+
+class Sample extends Demo
+{
+	float b=200.5f;
+	void m2()
+	{
+		System.out.println("child method" + this.b);
+	}
+}
+
+class Example25
+{
+	public static void main(String[] args)
+	{
+		Demo obj = new Sample();
+
+		obj.m1();
+	}
+}
