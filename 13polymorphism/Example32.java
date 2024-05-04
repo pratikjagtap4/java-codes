29638a91497fe408f114fd516bddeb36e4942e35
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-05-04 17:19:18 +0530

    overriding

---
+++ 13polymorphism/Example32.java
@@ -0,0 +1,24 @@
+class Parent
+{
+	private void m1()
+	{
+		System.out.println("parent m1");
+	}
+}
+
+class Child extends Parent
+{
+	void m1()
+	{
+		System.out.println("child m1");
+	}
+}
+
+class Example32
+{
+	public static void main(String[] args)
+	{
+		Parent p = new Child();
+		p.m1();
+	}
+}
