d0ea656454715f431e688ce040f550947e1d32dc
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-05-06 16:58:16 +0530

    abstraction

---
+++ 14abstraction/Example04.java
@@ -0,0 +1,17 @@
+abstract class Demo
+{
+	abstract void m1();
+
+	void m2()
+	{
+		System.out.println("m2 - complete method within abstract class");
+	}
+}
+
+class Example04
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
