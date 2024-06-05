d2cd1459df1fa0c86263b4051a11fa3a74bbdbbb
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-06-05 22:09:53 +0530

    nested classes

--- 19nested classes/Example3.java
+++ 19nested classes/Example3.java
@@ -1,1 +1,20 @@
+class Outer
+{
+	int a = 10;
+
+	class Inner
+	{
+		float b = 20.5f;
+		void m1()
+		{
+			System.out.println(a+b);
+		}
+	}
+}
 class Example3
+{
+	public static void main(String[] args)
+	{
+		new Outer().new Inner().m1();
+	}
+}
